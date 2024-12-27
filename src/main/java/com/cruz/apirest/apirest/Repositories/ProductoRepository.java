e17356f967fd935cce6a46eed46aa3766b937a91
AquilinoC <aquilinocruzm@outlook.com>
2024-12-27 09:36:43 -0600

    Realizamos un CRUD servido a través de una API REST realizada en JAVA utilizando Spring Boot, Hibernate y PostgreSQL

---
+++ src/main/java/com/cruz/apirest/apirest/Repositories/ProductoRepository.java
@@ -0,0 +1,9 @@
+package com.cruz.apirest.apirest.Repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.cruz.apirest.apirest.Entities.Producto;
+
+public interface ProductoRepository extends JpaRepository<Producto, Long> {
+
+}
